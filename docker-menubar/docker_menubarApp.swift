7a206345c2c735d7feabe550ca15215ba55aa157
Alireza Doostimehr <alirezadoostimehr@gmail.com>
2024-08-02 17:37:31 +0330

    Initial Commit

---
+++ docker-menubar/docker_menubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  docker_menubarApp.swift
+//  docker-menubar
+//
+//  Created by Alireza on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct docker_menubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
